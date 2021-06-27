da02810766e641b43dc659377bfd437588245752
Wilfried Klaas <w.klaas@gmx.de>
2021-06-27 16:21:14 +0200

    automated build system
    - adding version
    - adding ATtIny84 variants

--- builder/build.cmd
+++ builder/build.cmd
@@ -1,2 +1,4 @@
 docker build ./ -t arduino-tps-builder
-docker run -it arduino-tps-builder bash
+docker run --name arduino-tps-builder arduino-tps-builder bash
+docker cp arduino-tps-builder:/home/arduinocli/Arduino_TPS/dest/ ../
+docker stop arduino-tps-builder
